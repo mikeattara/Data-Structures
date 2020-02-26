c3e3b7929d0d1c817af45175ee88c0d212157c19
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-02-26 14:08:13 +0000

    copy files

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,7 +1,5 @@
-import sys
-sys.path.append('../queue_and_stack')
-from dll_queue import Queue
-from dll_stack import Stack
+from stack import Stack
+from queue import Queue
 
 
 class BinarySearchTree:
