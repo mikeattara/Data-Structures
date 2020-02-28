39504434a135a4b1bb2b0e5d5ee06c71dc8c71ed
Mike Perry Y Attara <mpyebattara@gmail.com>
2020-02-28 06:33:15 +0000

    remove test data

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -125,15 +125,3 @@ class BinarySearchTree:
     # Print Post-order recursive DFT
     def post_order_dft(self, node):
         pass
-
-
-bst = BinarySearchTree(1)
-bst.insert(8)
-bst.insert(5)
-bst.insert(7)
-bst.insert(6)
-bst.insert(3)
-bst.insert(4)
-bst.insert(2)
-
-bst.in_order_print(bst)
